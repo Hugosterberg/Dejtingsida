4413e7ff33ec5334ecc70623a99d978bdb6eda93
Hugosterberg <59054135+Hugosterberg@users.noreply.github.com>
2020-01-07 16:08:21 +0100

    fix

---
+++ Dejtingsida/Models/Profil.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Dejtingsida.Models
+{
+    public class Profil
+    {
+        [Key]
+        public string AnvändarID { get; set; }
+        public string Förnamn { get; set; }
+        public string Efternamn { get; set; }
+        public DateTime Födelsedatum { get; set; }
+        public string BildURL { get; set; }
+        public string Bio { get; set; }
+    }
+}
